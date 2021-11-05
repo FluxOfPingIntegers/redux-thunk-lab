5253f716586043126c1382f0ef0e79adbe559e81
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-11-05 13:55:30 -0400

    commit tree 5c7c9ccdae0515bca6f7a42d8ae63587d31e20b9

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,15 @@
-// write your CatList component here
+import React from "react"
+
+const CatList = props => {
+  let cats = (<li>Loading...</li>)
+  if (!props.loading) {
+    cats = props.catPics.map(cat => { return <li key={cat.id}><img src={cat.url} /></li>})
+  }
+  return (
+    <ul>
+      {cats}
+    </ul>
+  )
+}
+
+export default CatList
